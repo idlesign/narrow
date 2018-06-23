4acded70765ce64b301d0e05c99ec5620e5fb444
idle sign <idlesign@yandex.ru>
2018-06-23 19:10:30 +0700

    Pure py made default.

--- narrow/apps/__init__.py
+++ narrow/apps/__init__.py
@@ -1,6 +1,6 @@
 from ._base import APPS
+from .py import PurePy  # This should be first as default
 from .bottle import Bottle
 from .cherrypy import Cherrypy
 from .django import Django
 from .flask import Flask
-from .py import PurePy
