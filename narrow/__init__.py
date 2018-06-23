bc5f3dd37b93226b3bc7a056cbe2f508c1dc077e
idle sign <idlesign@yandex.ru>
2018-06-23 20:36:21 +0700

    Release v0.1.0

--- narrow/__init__.py
+++ narrow/__init__.py
@@ -7,10 +7,9 @@ from .shortcuts import gather_stats, stand_run
 from .stands import STANDS
 
 
-VERSION = (0, 0, 0)
+VERSION = (0, 1, 0)
 """Application version number tuple."""
 
 VERSION_STR = '.'.join(map(str, VERSION))
 """Application version number string."""
-
 
